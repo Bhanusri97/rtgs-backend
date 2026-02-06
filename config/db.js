48e58bf3bcd5a618e8cd8b7bee12d60441604e04
Bhanusri97 <bhanushri@bringjal.in>
2026-02-06 10:39:12 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/calander");
+    await mongoose.connect("mongodb+srv://vvk7899:rARDubo6G2K8LXNv@rtgs-ai-assistant.ya7j3mc.mongodb.net/?appName=Rtgs-AI-Assistant");
     console.log("MongoDB connected");
   } catch (err) {
     console.error("MongoDB connection error:", err);
